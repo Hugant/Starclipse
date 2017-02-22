ee741df24c3c103c855abdc7aa707442176b5d35
Hugant <HugantMD@yandex.ru>
2017-02-23 02:55:08 +0700

    Fixed needed resources

--- Building/StarshipsFactory/StarshipsFactory.java
+++ Building/StarshipsFactory/StarshipsFactory.java
@@ -6,13 +6,13 @@ public class StarshipsFactory {
 	private String name = "Starships Factory";
 	private Starship starship = null;
 	private int manpower = 0;
-	private Resources needResources = new Resources("iron",   "10000",   
-													"gold",   "5000",   
-													"tree",   "1000",
-													"stone",  "1000",    
-													"energy", "100000", 
-													"water",  "10000",
-													"money",  "10000000");
+	private Resources needResources = new Resources("iron",   "10K",   
+													"gold",   "5K",   
+													"tree",   "1K",
+													"stone",  "1K",    
+													"energy", "1M", 
+													"water",  "10K",
+													"money",  "10M");
 	
 	public void createStarship(String name, Cost cost, String... cells) {
 		this.starship = new Starship(name, cost, cells);
