c589d0b83585f034e88ff9f56263a19172bcbd22
Hugant <HugantMD@yandex.ru>
2017-02-28 23:19:44 +0700

    Add variables

--- Time.java
+++ Time.java
@@ -1,4 +1,10 @@
 package Starclipse;
 
 public class Time {
+	final static long DAY = 86400000L;
+	final static long HOUR = 3600000L;
+	final static long MINUTE = 60000L;
+	final static long SECOND = 1000L;
+	long duration = 0;
+	long start = 0;
 }
