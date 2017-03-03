51e9e69d5dd3d8a4f1c5135fd71957a6c49706ad
Hugant <HugantMD@yandex.ru>
2017-03-03 23:16:22 +0700

    Modificate constructor

--- Building/StarshipsFactory/Starship.java
+++ Building/StarshipsFactory/Starship.java
@@ -11,7 +11,7 @@ public class Starship {
 	private int speed = 0;
 	private int passenger = 0;
 	private Cost cost = null;
-		
+		 
 	private int residents = 0;
 	private int manpower = 5;// 2(pilots) , 3(mechanics)
 	
@@ -19,43 +19,39 @@ public class Starship {
 	private Resources expenses = null;
 	
 	public Starship(String name, Cost cost, String... cells) {
-		if(cells.length != 5) {
-			throw new IllegalArgumentException("The number of elements in the string array must be equal to 5");
-		} else {
-			for(String i : cells) {
-				switch(i) {
-					case "damage":
-						this.damage++;
-						this.manpower++;
-						break;
-						
-					case "speed":
-						this.speed++;
-						this.manpower++;
-						break;
-						
-					case "passenger":
-						this.passenger++;
-						this.manpower += 3;
-						break;
+		for(String i : cells) {
+			switch(i) {
+				case "damage":
+					this.damage++;
+					this.manpower++;
+					break;
 					
-					case "cost":
-						if(cost == null) {
-							throw new IllegalArgumentException("If you specify an element of 'cost', "
-																+ "the object cannot be null");
-						} else {
-							this.cost = cost;
-							this.manpower += cost.getManpower();
-						}
-						break;
-						
-					default:
-						throw new UnsupportedOperationException("Such a resource does not exist");
-				}
+				case "speed":
+					this.speed++;
+					this.manpower++;
+					break;
+					
+				case "passenger":
+					this.passenger++;
+					this.manpower += 3;
+					break;
 				
-				this.name = name;
-				this.residents = passenger * PASSENGER_VOLUME;
+				case "cost":
+					if(cost == null) {
+						throw new IllegalArgumentException("If you specify an element of 'cost', "
+															+ "the object cannot be null");
+					} else {
+						this.cost = cost;
+						this.manpower += cost.getManpower();
+					}
+					break;
+					
+				default:
+					throw new UnsupportedOperationException("Such a resource does not exist");
 			}
+			
+			this.name = name;
+			this.residents = passenger * PASSENGER_VOLUME;
 		}
 	}
 	
