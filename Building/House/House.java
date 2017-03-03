e0b9e8fc1619d07e1e793dcf914c419377375e58
Hugant <HugantMD@yandex.ru>
2017-03-03 23:22:28 +0700

    Modificate some parts of the code

--- Building/House/House.java
+++ Building/House/House.java
@@ -14,44 +14,42 @@ public class House {
 	
 	public House(String type) {
 		switch(type) {
+		switch(type.toLowerCase()) {
 			case "big":
 				this.maxResidents = 100;
 				this.name = "Big House";
-				//this.incomeTime;
-				//this.buildingTime;
-				this.needResources = new Resources("stone",  "100", 
-												   "tree",   "50", 
-												   "money",  "1000", 
-												   "iron",   "10",  
-												   "water",  "10", 
-												   "energy", "10"  );
-				break;
+				this.buildTime = new Timer(Timer.MINUTE + Timer.SECOND * 30);
+				this.expenses = new Resources("stone",  "100", 
+											  "tree",   "50", 
+											  "money",  "1000", 
+											  "iron",   "10",  
+											  "water",  "10", 
+											  "energy", "10"  );
+				return;
 				
 			case "avarage":
 				this.maxResidents = 50;
 				this.name = "Avarage House";
-				//this.incomeTime;
-				//this.buildingTime;
-				this.needResources = new Resources("stone",  "50", 
-												   "tree",   "25", 
-												   "money",  "500",
-												   "iron",   "5" , 
-												   "water",  "5" , 
-												   "energy", "5"  );	
-				break;
+				this.buildTime = new Timer(Timer.MINUTE * 5);
+				this.expenses = new Resources("stone",  "50", 
+											  "tree",   "25", 
+											  "money",  "500",
+											  "iron",   "5" , 
+											  "water",  "5" , 
+											  "energy", "5"  );	
+				return;
 				
 			case "small":
 				this.maxResidents = 10;
 				this.name = "Small House";
-				//this.incomeTime;
-				//this.buildingTime;
-				this.needResources = new Resources("stone",  "12", 
-												   "tree",   "6", 
-												   "money",  "125",
-												   "iron",   "1" , 
-												   "water",  "1", 
-												   "energy", "1"  );
-				break;
+				this.buildTime = new Timer(Timer.MINUTE * 30);
+				this.expenses = new Resources("stone",  "12", 
+											  "tree",   "6", 
+											  "money",  "125",
+											  "iron",   "1" , 
+											  "water",  "1", 
+											  "energy", "1"  );
+				return;
 		}
 	}
 }
