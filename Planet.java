059d35da70424511044f34af5fb41f414f05fe0c
Hugant <HugantMD@yandex.ru>
2017-03-06 23:03:35 +0700

    Create class

---
+++ Planet.java
@@ -0,0 +1,18 @@
+package starclipse;
+
+import java.util.ArrayList;
+
+import starclipse.building.Building;
+import starclipse.building.Resources;
+
+public class Planet {
+	Resources resources;
+	ArrayList<Building> infrastructure = new ArrayList<Building>();
+	ArrayList<Planet> travel = new ArrayList<Planet>();
+	
+	public Planet() {
+		
+	}
+	
+	
+}
