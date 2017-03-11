ea840a11ce10fde172d277c211d93ee32c960aa5
Hugant <HugantMD@yandex.ru>
2017-03-11 16:41:58 +0700

    Documentation work

--- common/Subject.java
+++ common/Subject.java
@@ -97,9 +97,12 @@ public class Subject {
 	}
 	
 	/**
-	 * Return the string in the form LimitlessNumber.
+	 * Return the string in the form LimitlessNumber rounded down to the nearest tenth.
 	 * <br>
 	 * <pre><code> new Subject("145T").getNumber();// "145T"
+	 *  new Subject("1.001V").getNumber();// "1V"
+	 *  new Subject("1.01Z").getNumber();// "1Z"
+	 *  new Subject("1.1234J").getNumber();// "1.12J"
 	 * </pre><code>
 	 * @return string in the form LimitlessNumber
 	 */
