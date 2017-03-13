2b2cd168e93930189caa1db12ba27a42c981e896
Hugant <HugantMD@yandex.ru>
2017-03-13 23:01:45 +0700

    Create class

---
+++ Infrastructure.java
@@ -0,0 +1,8 @@
+package starclipse;
+
+import java.util.ArrayList;
+import starclipse.building.Building;
+
+public class Infrastructure {
+	private ArrayList<Building> buildings = new ArrayList<Building>();
+}
