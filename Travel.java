5e8ac5fea1aa87a83a591e7fddccee0270f13ce7
Hugant <HugantMD@yandex.ru>
2017-03-13 23:01:18 +0700

    Create class

---
+++ Travel.java
@@ -0,0 +1,11 @@
+package starclipse;
+
+import java.util.ArrayList;
+
+public class Travel {
+	private ArrayList<Planet> planets = new ArrayList<Planet>();
+	
+	public void travelTo(String name) {
+		
+	}
+}
