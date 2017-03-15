01b5d313792af7a9bd0bec6008708ccbee0712d7
Hugant <HugantMD@yandex.ru>
2017-03-15 23:26:31 +0700

    Test

--- building/industry/Starship.java
+++ building/industry/Starship.java
@@ -81,41 +81,42 @@ public class Starship {
 	private String name = "";
 	private Resources expenses = null;
 	
-	public Starship(String name, Resources res, String... cells) {
-		for(String i : cells) {
-			switch(i) {
-				case "damage":
-					this.damage++;
-					this.manpower++;
-					break;
-					
-				case "speed":
-					this.speed++;
-					this.manpower++;
-					break;
-					
-				case "passenger":
-					this.passenger++;
-					this.manpower += 3;
-					break;
-				
-				case "cost":
-					if(cost == null) {
-						throw new IllegalArgumentException("If you specify an element of 'cost', "
-															+ "the object cannot be null");
-					} else {
-						this.cost = new Cost("");
-						this.manpower += cost.getManpower();
-					}
-					break;
-					
-				default:
-					throw new UnsupportedOperationException("Such a resource does not exist");
-			}
-			
-			this.name = name;
-			this.residents = passenger * PASSENGER_VOLUME;
-		}
+	public Starship(String name, Resources res/*, String... cells*/) {
+		Storage a = new Storage(res);
+//		for(String i : cells) {
+//			switch(i) {
+//				case "damage":
+//					this.damage++;
+//					this.manpower++;
+//					break;
+//					
+//				case "speed":
+//					this.speed++;
+//					this.manpower++;
+//					break;
+//					
+//				case "passenger":
+//					this.passenger++;
+//					this.manpower += 3;
+//					break;
+//				
+//				case "cost":
+//					if(cost == null) {
+//						throw new IllegalArgumentException("If you specify an element of 'cost', "
+//															+ "the object cannot be null");
+//					} else {
+//						this.cost = new Cost("");
+//						this.manpower += cost.getManpower();
+//					}
+//					break;
+//					
+//				default:
+//					throw new UnsupportedOperationException("Such a resource does not exist");
+//			}
+//			
+//			this.name = name;
+//			this.residents = passenger * PASSENGER_VOLUME;
+//		}
 	}
 	
 	public int getResidents() {
