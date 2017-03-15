0273522352abdafa55266182f954b2a9d47b6a0c
Hugant <HugantMD@yandex.ru>
2017-03-15 23:22:33 +0700

    Modificate variable

--- Building/Warehouse/Warehouse.java
+++ Building/Warehouse/Warehouse.java
@@ -6,7 +6,7 @@ import starclipse.common.Subject;
 
 public class Warehouse /*implements starclipse.building.Building*/{
 	
-	private Subject residents = new Subject("residents", "10", "70");
+	private Subject residents = new Subject(Subject.RESIDENTS, "10", "70");
 	private LimitlessNumber volume = new LimitlessNumber("500");// the amount of resources per unit 
 	private Resources store;
 	private Resources expenses = null;
