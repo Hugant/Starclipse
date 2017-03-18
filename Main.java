fbd16d2e0979e5a631779c9fba4a471c4a23c349
Hugant <HugantMD@yandex.ru>
2017-03-18 17:11:48 +0700

    Test

--- Main.java
+++ Main.java
@@ -18,33 +18,34 @@ import starclipse.common.Subject;
 
 public class Main{
 	public static void main(String args[]) {
-			
-//		House a = new House("small");
-//		a.build();
-//		String c = "";
-//		while(true) {
-//			c = a.getStatus();
-//			//System.out.println(c);
-//			if(c.equals("Claim")){
-//				a.claim();
-//			}
-//			try {
-//				Thread.sleep(100);
-//			} catch(Exception e) {}
-//		}
+		String [] mas = {""};
+		House a = new House("small");
+		a.startWork();
+		String c = "";
+		while(true) {
+			c = a.getStatus();
+			System.out.println(c);
+			if(c.equals("End Work")) {
+				//new java.util.Scanner(System.in).nextLine();
+				a.claim();
+			}
+			try {
+				Thread.sleep(500);
+			} catch(Exception e) {}
+		}
 		
 //		Resources a = new Resources(new LimitlessNumber("100"), "gold", "oxygen", "gold");
 //		Starship b = new Starship("Hugant", a);
 		//System.out.println(Arrays.asList(a.asTypeArray()));
 		
-		Starship a = new Starship("", new Resources(new LimitlessNumber("100M"), "gold", "iron", "oxygen", "coal", "energy"), 
-				"damage","speed", "storage", "speed", "damage");
-		
-//		Starship a = new Starship("Hugant", null, "speed", "speed", "damage", "damage", "passenger");
-		
-		System.out.println(a.getName());
-		System.out.println("damage: " + a.getDamage());
-		System.out.println("speed: " + a.getSpeed());
-		System.out.println("passenger: " + a.getPassenger());
+//		Starship a = new Starship("", new Resources(new LimitlessNumber("100M"), "gold", "iron", "oxygen", "coal", "energy"), 
+//				"damage","speed", "storage", "speed", "damage");
+//		
+////		Starship a = new Starship("Hugant", null, "speed", "speed", "damage", "damage", "passenger");
+//		
+//		System.out.println(a.getName());
+//		System.out.println("damage: " + a.getDamage());
+//		System.out.println("speed: " + a.getSpeed());
+//		System.out.println("passenger: " + a.getPassenger());
 	}
 }
