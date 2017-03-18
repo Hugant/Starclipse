deb6bfce6bdc6b5f3c87d7d57eb4337930a9c477
Hugant <HugantMD@yandex.ru>
2017-03-18 20:35:10 +0700

    Test

--- Building/Warehouse/Warehouse.java
+++ Building/Warehouse/Warehouse.java
@@ -4,7 +4,7 @@ import starclipse.building.Resources;
 import starclipse.common.LimitlessNumber;
 import starclipse.common.Subject;
 
-public class Warehouse /*implements starclipse.building.Building*/{
+public class Warehouse extends starclipse.building.Building {
 	
 	private Subject residents = new Subject(Subject.RESIDENTS, "10", "70");
 	private LimitlessNumber volume = new LimitlessNumber("500");// the amount of resources per unit 
