c0447fc55460ff740d70c9e9ac4afaf72f57b767
Hugant <HugantMD@yandex.ru>
2017-03-20 23:36:46 +0700

    Test

--- Building/House/House.java
+++ Building/House/House.java
@@ -14,33 +14,33 @@ public class House extends starclipse.building.Building {
 	
 	public House(String type) {
 		switch (type.toLowerCase()) {
-			case "big":
-				this.residents = new Subject(Subject.RESIDENTS, "0", "100");
-				this.name = "Big House";
-				this.buildTime = new Timer(Timer.MINUTE + Timer.SECOND * 30, "Start");
-				this.incomeTime = new Timer(Timer.SECOND * 20, "Claim");
-				this.income = new Resources(Subject.RESIDENTS, "10", Subject.MONEY, "100");
-				this.expenses = new Resources(Subject.STONE,  "100", 
-											  Subject.TREE,   "50", 
-											  Subject.MONEY,  "1000", 
-											  Subject.IRON,   "10",  
-											  Subject.WATER,  "10", 
-											  Subject.ENERGY, "10"  );
-				return;
-				
-			case "avarage":
-				this.residents = new Subject(Subject.RESIDENTS, "0", "50");
-				this.name = "Avarage House";
-				this.buildTime = new Timer(Timer.MINUTE * 5, "Start");
-				this.incomeTime = new Timer(Timer.SECOND * 20, "Claim");
-				this.income = new Resources(Subject.RESIDENTS, "5", Subject.MONEY, "50");
-				this.expenses = new Resources(Subject.STONE,  "50", 
-											  Subject.TREE,   "25", 
-											  Subject.MONEY,  "500",
-											  Subject.IRON,   "5" , 
-											  Subject.WATER,  "5" , 
-											  Subject.ENERGY, "5"  );	
-				return;
+//			case "big":
+//				this.residents = new Subject(Subject.RESIDENTS, "0", "100");
+//				super.setName("Big House");
+//				this.buildTime = new Timer(Timer.MINUTE + Timer.SECOND * 30, "Start");
+//				this.incomeTime = new Timer(Timer.SECOND * 20, "Claim");
+//				this.income = new Resources(Subject.RESIDENTS, "10", Subject.MONEY, "100");
+//				this.expenses = new Resources(Subject.STONE,  "100", 
+//											  Subject.TREE,   "50", 
+//											  Subject.MONEY,  "1000", 
+//											  Subject.IRON,   "10",  
+//											  Subject.WATER,  "10", 
+//											  Subject.ENERGY, "10"  );
+//				return;
+//				
+//			case "avarage":
+//				this.residents = new Subject(Subject.RESIDENTS, "0", "50");
+//				this.name = "Avarage House";
+//				this.buildTime = new Timer(Timer.MINUTE * 5, "Start");
+//				this.incomeTime = new Timer(Timer.SECOND * 20, "Claim");
+//				this.income = new Resources(Subject.RESIDENTS, "5", Subject.MONEY, "50");
+//				this.expenses = new Resources(Subject.STONE,  "50", 
+//											  Subject.TREE,   "25", 
+//											  Subject.MONEY,  "500",
+//											  Subject.IRON,   "5" , 
+//											  Subject.WATER,  "5" , 
+//											  Subject.ENERGY, "5"  );	
+//				return;
 				
 			case "small":
 				this.residents = new Subject(Subject.RESIDENTS, "0", "10");
