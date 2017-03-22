7b162d9a2b065d62ed63ba7020e57ac70bf48f93
Hugant <HugantMD@yandex.ru>
2017-03-22 21:07:13 +0700

    Modificate constructor

--- Building/House/House.java
+++ Building/House/House.java
@@ -15,13 +15,6 @@ import starclipse.common.Subject;
  *
  */
 public class House extends starclipse.building.Building {
-//	private String name = "";
-//	private Timer buildTime;
-//	private Timer incomeTime;
-//	private Subject residents;
-//	private Resources income = null;
-//	private Resources expenses = null;
-	
 	/**
 	 * Create a <b>House</b> where the parameters specified a type
 	 * of <b>House</b>.
@@ -29,33 +22,33 @@ public class House extends starclipse.building.Building {
 	 */
 	public House(String type) {
 		switch (type.toLowerCase()) {
-//			case "big":
-//				this.residents = new Subject(Subject.RESIDENTS, "0", "100");
-//				super.setName("Big House");
-//				this.buildTime = new Timer(Timer.MINUTE + Timer.SECOND * 30, "Start");
-//				this.incomeTime = new Timer(Timer.SECOND * 20, "Claim");
-//				this.income = new Resources(Subject.RESIDENTS, "10", Subject.MONEY, "100");
-//				this.expenses = new Resources(Subject.STONE,  "100", 
-//											  Subject.TREE,   "50", 
-//											  Subject.MONEY,  "1000", 
-//											  Subject.IRON,   "10",  
-//											  Subject.WATER,  "10", 
-//											  Subject.ENERGY, "10"  );
-//				return;
-//				
-//			case "avarage":
-//				this.residents = new Subject(Subject.RESIDENTS, "0", "50");
-//				this.name = "Avarage House";
-//				this.buildTime = new Timer(Timer.MINUTE * 5, "Start");
-//				this.incomeTime = new Timer(Timer.SECOND * 20, "Claim");
-//				this.income = new Resources(Subject.RESIDENTS, "5", Subject.MONEY, "50");
-//				this.expenses = new Resources(Subject.STONE,  "50", 
-//											  Subject.TREE,   "25", 
-//											  Subject.MONEY,  "500",
-//											  Subject.IRON,   "5" , 
-//											  Subject.WATER,  "5" , 
-//											  Subject.ENERGY, "5"  );	
-//				return;
+			case "big":
+				super.setResidents(new Subject(Subject.RESIDENTS, "0", "100"));
+				super.setName("Big House");
+				super.setBuildTimer(new Timer(Timer.MINUTE * 30, "Start"));
+				super.setIncomeTimer(new Timer(Timer.SECOND * 20, "Claim"));
+				super.setIncome(new Resources(Subject.RESIDENTS, "10", Subject.MONEY, "100"));
+				super.setExpenses(new Resources(Subject.STONE,  "100", 
+											  	Subject.TREE,   "50", 
+											  	Subject.MONEY,  "1000", 
+											  	Subject.IRON,   "10",  
+											  	Subject.WATER,  "10", 
+											  	Subject.ENERGY, "10"  ));
+				return;
+				
+			case "average":
+				super.setResidents(new Subject(Subject.RESIDENTS, "0", "50"));
+				super.setName("Average House");
+				super.setBuildTimer(new Timer(Timer.MINUTE * 5, "Start"));
+				super.setIncomeTimer(new Timer(Timer.SECOND * 20, "Claim"));
+				super.setIncome(new Resources(Subject.RESIDENTS, "5", Subject.MONEY, "50"));
+				super.setExpenses(new Resources(Subject.STONE,  "50", 
+											  	Subject.TREE,   "25", 
+											  	Subject.MONEY,  "500",
+											  	Subject.IRON,   "5" , 
+											  	Subject.WATER,  "5" , 
+											  	Subject.ENERGY, "5"  ));	
+				return;
 				
 			case "small":
 				super.setResidents(new Subject(Subject.RESIDENTS, "0", "10"));
@@ -64,11 +57,11 @@ public class House extends starclipse.building.Building {
 				super.setIncomeTimer(new Timer(Timer.SECOND * 10, "Claim"));
 				super.setIncome(new Resources(Subject.RESIDENTS, "3", Subject.MONEY, "25"));
 				super.setExpenses(new Resources(Subject.STONE,  "12", 
-											  Subject.TREE,   "6", 
-											  Subject.MONEY,  "125",
-											  Subject.IRON,   "1" , 
-											  Subject.WATER,  "1", 
-											  Subject.ENERGY, "1"  ));
+												Subject.TREE,   "6", 
+											  	Subject.MONEY,  "125",
+											  	Subject.IRON,   "1" , 
+											  	Subject.WATER,  "1", 
+											  	Subject.ENERGY, "1"  ));
 				return;
 		}
 	}
