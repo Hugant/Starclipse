09c819e3f031f252f7a12f23f1a9578ad5105cbe
Hugant <HugantMD@yandex.ru>
2017-03-22 23:54:06 +0700

    Remove unnecessary statements

--- building/Warehouse.java
+++ building/Warehouse.java
@@ -6,10 +6,8 @@ import starclipse.common.Subject;
 
 public class Warehouse extends starclipse.building.Building {
 	
-	private Subject residents = new Subject(Subject.RESIDENTS, "10", "70");
 	private LimitlessNumber volume = new LimitlessNumber("500");// the amount of resources per unit 
 	private Resources store;
-	private Resources expenses = null;
 	
 	public Warehouse(String... cells) {
 		if(cells.length != 6) {
