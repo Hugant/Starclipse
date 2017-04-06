cba48d534d735dc378ff17257a876f5210ac49d7
Hugant <HugantMD@yandex.ru>
2017-04-06 23:19:27 +0700

    Add static variable

--- building/industry/Starship.java
+++ building/industry/Starship.java
@@ -57,6 +57,7 @@ public class Starship {
 	private static final String NAMES_MAS[] = {"Crimson", "Wenda", "Lexa"};
 	private static final ScaleNumber FOOD_FOR_ONE_PASSENGER = new ScaleNumber("50");
 	private static final ScaleNumber OXYGEN_FOR_ONE_PASSENGER = new ScaleNumber("100");
+	private static final ScaleNumber MONEY_FOR_ONE_PASSENGER = new ScaleNumber("500");
 	
 	private int damage = 0;
 	private int speed = 0;
