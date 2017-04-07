13770d71d0278089c3a10bffb173751d4bee1dfd
Hugant <HugantMD@yandex.ru>
2017-04-07 23:25:54 +0700

    Modified constructor

--- building/industry/ResourcesFactory.java
+++ building/industry/ResourcesFactory.java
@@ -11,70 +11,87 @@ public class ResourcesFactory extends starclipse.building.Building {
 	private static ScaleNumber maxResidents = new ScaleNumber("1K");
 	private Resources expenses = new Resources();
 	
-	public ResourcesFactory(String... cells) {
-		if (cells.length == 5) {
-			for (String i : cells) {
-				switch (i) {
-					case Subject.TREE:
-						expenses.add(new Resources(	Subject.SOIL, 	"",
-													Subject.WATER, 	"",
-													Subject.OXYGEN, "",
-													Subject.MONEY, 	""));
-						super.setIncome(new Resources(Subject.TREE,	""));
-						break;
-					
-					case Subject.STONE:
-						expenses.add(new Resources(	Subject.SOIL,	"",
-													Subject.TREE,	"",
-													Subject.MONEY,	""));
-						break;
-						
-					case Subject.COAL:
-						expenses.add(new Resources(	Subject.STONE,	"",
-													Subject.WATER,	"",
-													Subject.MONEY,	""));
-						break;
-						
-					case Subject.IRON:
-						expenses.add(new Resources(	Subject.STONE,	"",
-													Subject.MONEY,	""));
-						break;
-						
-					case Subject.GOLD:
-						expenses.add(new Resources());
-						break;
-						
-					case Subject.ENERGY:
-						expenses.add(new Resources());
-						break;
-						
-					case Subject.WATER:
-						expenses.add(new Resources());
-						break;
-						
-					case Subject.OXYGEN:
-						expenses.add(new Resources());
-						break;
-						
-					case Subject.FOOD:
-						expenses.add(new Resources());
-						break;
-						
-					case Subject.SOIL:
-						expenses.add(new Resources(	Subject.STONE, 	"",
-													Subject.WATER,	"",
-													Subject.OXYGEN,	"",
-													Subject.MONEY,	""));
-						break;
-						
-					default:
-						throw new IllegalArgumentException("Such a resource does not exist");
-				}
+	
+	public ResourcesFactory(String type) {
+		super.setName(type.substring(0, 1).toUpperCase() + type.substring(1).toLowerCase() + " Factory");
+		super.setBuildTimer(Timer.DAY * 4, "Start");
+		super.setIncomeTimer(Timer.DAY, "Claim");
+		super.setResidents(new Subject(Subject.RESIDENTS, "0", maxResidents.toString()));
+		
+		switch (type) {
+			case Subject.TREE:
+				expenses.add(new Resources(	Subject.SOIL, 	"",
+											Subject.WATER, 	"",
+											Subject.OXYGEN, "",
+											Subject.MONEY, 	""));
+				super.setIncome(new Resources(
+					Subject.TREE, new ScaleNumber(super.getResidents()).multiply(INCOME_PER_RESIDENT).toString()));
+				break;
+			
+			case Subject.STONE:
+				expenses.add(new Resources(	Subject.SOIL,	"",
+											Subject.TREE,	"",
+											Subject.MONEY,	""));
+				super.setIncome(new Resources(
+						Subject.STONE, new ScaleNumber(super.getResidents()).multiply(INCOME_PER_RESIDENT).toString()));
+				break;
 				
-				super.setName(i.substring(0, 1).toUpperCase() + i.substring(1).toLowerCase() + " Factory");
-			}
-		} else {
-			throw new IllegalArgumentException("The amount of cells must equal 5");
+			case Subject.COAL:
+				expenses.add(new Resources(	Subject.STONE,	"",
+											Subject.WATER,	"",
+											Subject.MONEY,	""));
+				super.setIncome(new Resources(
+						Subject.COAL, new ScaleNumber(super.getResidents()).multiply(INCOME_PER_RESIDENT).toString()));
+				break;
+				
+			case Subject.IRON:
+				expenses.add(new Resources(	Subject.STONE,	"",
+											Subject.MONEY,	""));
+				super.setIncome(new Resources(
+						Subject.IRON, new ScaleNumber(super.getResidents()).multiply(INCOME_PER_RESIDENT).toString()));
+				break;
+				
+			case Subject.GOLD:
+				expenses.add(new Resources());
+				super.setIncome(new Resources(
+						Subject.GOLD, new ScaleNumber(super.getResidents()).multiply(INCOME_PER_RESIDENT).toString()));
+				break;
+				
+			case Subject.ENERGY:
+				expenses.add(new Resources());
+				super.setIncome(new Resources(
+						Subject.ENERGY, new ScaleNumber(super.getResidents()).multiply(INCOME_PER_RESIDENT).toString()));
+				break;
+				
+			case Subject.WATER:
+				expenses.add(new Resources());
+				super.setIncome(new Resources(
+						Subject.WATER, new ScaleNumber(super.getResidents()).multiply(INCOME_PER_RESIDENT).toString()));
+				break;
+				
+			case Subject.OXYGEN:
+				expenses.add(new Resources());
+				super.setIncome(new Resources(
+						Subject.OXYGEN, new ScaleNumber(super.getResidents()).multiply(INCOME_PER_RESIDENT).toString()));
+				break;
+				
+			case Subject.FOOD:
+				expenses.add(new Resources());
+				super.setIncome(new Resources(
+						Subject.FOOD, new ScaleNumber(super.getResidents()).multiply(INCOME_PER_RESIDENT).toString()));
+				break;
+				
+			case Subject.SOIL:
+				expenses.add(new Resources(	Subject.STONE, 	"",
+											Subject.WATER,	"",
+											Subject.OXYGEN,	"",
+											Subject.MONEY,	""));
+				super.setIncome(new Resources(
+						Subject.SOIL, new ScaleNumber(super.getResidents()).multiply(INCOME_PER_RESIDENT).toString()));
+				break;
+				
+			default:
+				throw new IllegalArgumentException("Such a resource does not exist");
 		}
 	}
 }
