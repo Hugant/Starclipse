3e857a93635fd10930a53bbb5157bf249c74358b
Hugant <hugantmd@yandex.ru>
2017-04-12 08:27:15 +0700

    Test

--- Main.java
+++ Main.java
@@ -20,6 +20,12 @@ import starclipse.common.Subject;
 
 public class Main{
 	public static void main(String args[]) {
+//		Resources a = new Resources(Subject.GOLD, "100", Subject.IRON, "40");
+//		System.out.println(a.divide("10").get(Subject.GOLD));
+//		System.out.println(a.get(Subject.IRON));
+//		Subject a = new Subject(Subject.GOLD, "100", "200");
+//		Subject b = new Subject(Subject.GOLD, "2.5");
+//		System.out.println(a.multiply(b).getNumber());
 		//System.out.println(Subject.TREE.substring(0, 1).toUpperCase() + Subject.TREE.substring(1) + " Factory");
 //		Starship a = new Starship("Hugant", new Resources(new ScaleNumber("2K"), "gold", "gold", "oxygen", "water", "stone"), "damage", "speed", "damage", "passenger", "storage");
 //		System.out.println(Arrays.asList(a.getExpenses().asTypeArray()));
@@ -39,29 +45,29 @@ public class Main{
 //		System.out.println(a);
 //		a.fill();
 //		System.out.println(a);
-		System.out.println("Test");
-		Building a = new House("small");
-		a.build();
-		
-		
-		String c = "";
-		while(true) {
-			c = a.getStatus();
-			System.out.println(c);
-			if (c.equals("Start")) {
-				a.startWork();
-			}
-			if(c.equals("Claim")) {
-				//new java.util.Scanner(System.in).nextLine();
-				a.claim();
-			}
-			try {
-				Thread.sleep(1000);
-			} catch(Exception e) {}
-			for(int i = 0; i < 10; i++) {
-				System.out.println();
-			}
-		}
+//		System.out.println("Test");
+//		Building a = new House("small");
+//		a.build();
+//		
+//		
+//		String c = "";
+//		while(true) {
+//			c = a.getStatus();
+//			System.out.println(c);
+//			if (c.equals("Start")) {
+//				a.startWork();
+//			}
+//			if(c.equals("Claim")) {
+//				//new java.util.Scanner(System.in).nextLine();
+//				a.claim();
+//			}
+//			try {
+//				Thread.sleep(1000);
+//			} catch(Exception e) {}
+//			for(int i = 0; i < 10; i++) {
+//				System.out.println();
+//			}
+//		}
 //		
 //		Resources a = new Resources(new LimitlessNumber("100"), "gold", "oxygen", "gold");
 //		Starship b = new Starship("Hugant", a);
