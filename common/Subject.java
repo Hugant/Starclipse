a22667aaade0cf4b2f89a4a31145824b6b602844
Hugant <hugantmd@yandex.ru>
2017-04-12 08:29:07 +0700

    Craete method multiply(Subject)

--- common/Subject.java
+++ common/Subject.java
@@ -167,6 +167,23 @@ public class Subject {
 	 * @param subject is a <b>Subject</b> which you want to multiply
 	 * @return this
 	 */
+	public Subject multiply(Subject subject) {
+		if (subject.type == null || this.type == null) {
+			this.value.multiply(subject.value);
+		} else if (this.type.equals(subject.type)) {
+			this.value.multiply(subject.value);
+		} else {
+			throw new ArithmeticException("Arithmetic operations can only be done with the same material");
+		}
+		
+		if (this.value.compareTo("0") == -1) {
+			throw new ArithmeticException("Out of bounds the minimum value");
+		} else if (this.maxValue != null && this.value.compareTo(this.maxValue) == 1) {
+			throw new ArithmeticException("Out of bounds the maximum value");
+		}
+		
+		return this;
+	}
 
 	/**
 	 * Divide a <b>Subject</b> to the <b>Subject</b>.
