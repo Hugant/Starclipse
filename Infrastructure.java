9d28e3c5f3a4f5a54d52567df193f8a2d01fe676
Hugant <hugantmd@yandex.ru>
2017-04-14 18:23:21 +0700

    Nothing

--- Infrastructure.java
+++ Infrastructure.java
@@ -1,7 +1,14 @@
 package starclipse;
 
 import java.util.ArrayList;
+import starclipse.building.*;
+import starclipse.common.ScaleNumber;
+import starclipse.common.Subject;
 
 public class Infrastructure {
-	private ArrayList<Building> buildings = new ArrayList<Building>();
+	private ArrayList<Building> infrastructure = new ArrayList<Building>();
+	
+	public Infrastructure() {
+		
+	}
 }
