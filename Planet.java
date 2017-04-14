4d105803a498e0d1ab3b4e578f22fab4e2420d70
Hugant <hugantmd@yandex.ru>
2017-04-14 18:23:10 +0700

    Modified class

--- Planet.java
+++ Planet.java
@@ -1,14 +1,25 @@
 package starclipse;
 
 import starclipse.common.Resources;
+import starclipse.common.Subject;
+import starclipse.building.House;
 
 public class Planet {
 	private Resources resources;
 	private Resources effect;
-	private Infrastructure infrastructure;
+	
+	//private Infrastructure infrastructure;
+	
+	private Building[] infrastructure = new Building[5];
+	private Subject residents;
+	
 	private String name;
+	private boolean defend = false;
+	
 	
 	public Planet(String name, Resources effect) {
-		
+		this.name = name;
+		this.effect = effect;
+		this.infrastructure[0] = new House("Small House");
 	}
 }
